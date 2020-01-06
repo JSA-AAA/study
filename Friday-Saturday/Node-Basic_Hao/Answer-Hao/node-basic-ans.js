4ce372c2e88ded0cd9dd606e40b600edaf9d353d
Hao <Hao_Yu@epam.com>
2020-01-06 10:41:18 +0800

    node-basic done

--- Friday-Saturday/Node-Basic_Hao/Answer-Hao/node-basic-ans.js
+++ Friday-Saturday/Node-Basic_Hao/Answer-Hao/node-basic-ans.js
@@ -30,10 +30,68 @@ setTimeout(() => {
 
 
 //--------------------------events--------------------------------
+// 1. are registered events executed synchronously or asynchronously?
+ synchronous
+
+// 2. base on your answer of Q1, give the output of below code
+const EventEmitter = require('events');
+class MyEmitter extends EventEmitter{};
+const myEmitter = new MyEmitter();
+myEmitter.on('event', function() {
+  console.log('listener1');
+});
+myEmitter.on('event', async function() {
+  console.log('listener2-1');
+  await new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('listener2-2');
+      resolve(1);
+    }, 1000);
+  });
+});
+myEmitter.on('event', function() {
+  console.log('listener3');
+});
+myEmitter.emit('event');
+console.log('end');
+
+// 作者：Randal
+// 链接：https://juejin.im/post/5b0189fe51882567161ad8ef
+// 来源：掘金
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 
 
+listener1
+listener2-1
+listener3
+end
+listener2-2
 
 
+// 3. give output of below code
+const EventEmitter = require('events');
+const myEE = new EventEmitter();
+myEE.once('foo', () => console.log('a'));
+myEE.prependOnceListener('foo', () => console.log('b'));
+myEE.emit('foo');
+myEE.emit('foo');
+myEE.emit('foo');
 
+b
+a
 
-//------------------------process----------------------------------
+// 3. where can we receive the return value of a event callback function.
+// for example, 100 in below code
+
+const EventEmitter = require('events');
+class MyEmitter extends EventEmitter {}
+const myEmitter = new MyEmitter();
+
+myEmitter.on('event',() => {
+  console.log('haha');
+  return 100;
+})
+
+myEmitter.emit('event');
+
+// can not, since return value of a event callback function will be ignored
--- Friday-Saturday/Node-Basic_Hao/README.md
+++ Friday-Saturday/Node-Basic_Hao/README.md
@@ -21,3 +21,5 @@
 ## Material
  - [Node.js Doc](https://nodejs.org/docs/latest-v11.x/api/)
  - [Node.js中文版](http://nodejs.cn/api/)
+ - [掘金：Node.js EventEmitter解读](https://juejin.im/post/5b0189fe51882567161ad8ef)
+ - [掘金：Node.js process解读](https://juejin.im/post/5b0e97bef265da0914072515)
