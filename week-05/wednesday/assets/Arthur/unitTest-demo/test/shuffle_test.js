0b67e0b79b41dc0c7e525c0b72df4e50e953dd46
大多 <eleven032@gmail.com>
2020-01-15 17:59:08 +0800

    unit test demo

---
+++ week-05/wednesday/assets/Arthur/unitTest-demo/test/shuffle_test.js
@@ -0,0 +1,23 @@
+const shuffle = require('../helpers/shuffle.js');
+const assert = require('assert');
+const expect = require("chai").expect;
+
+
+
+describe('shuffle_result', function () {
+  it('Array should not equal to the one after shuffled', function () {
+    assert.notEqual(shuffle([1, 2, 3, 4]), [1, 2, 3, 4]);
+  });
+});
+
+describe('shuffle_result_length', function () {
+  it('length should equal to the one after shuffled', function () {
+    assert.equal(shuffle([1, 2, 3, 4]).length, 4);
+  });
+});
+
+describe('shuffle_result_type', function () {
+  it('Type should be a number', function () {
+    expect(shuffle([1, 2, 3, 4]).indexOf(2)).is.a(typeof ([1, 2, 3, 4].indexOf(2)));
+  });
+});
